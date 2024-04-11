8b818c30579aeaba5f9d5e7c10482dd0516bd1c8
syedarhabhussain <syedarhabhussain>
2024-04-11 16:42:41 +0500

    Integrate

--- src/get_history.php
+++ src/get_history.php
@@ -3,7 +3,28 @@ global $conn;
 global $repository;
 @session_start();
 require_once ('db.php');
-require_once ('FuelQuoteRepository.php');
+
+class FuelQuoteRepository {
+    private $conn;
+    public function __construct($conn) {
+        $this->conn = $conn;
+    }
+    public function getFuelQuotesByUser($username) {
+        $fuelQuotes = [];
+        $stmt = $this->conn->prepare("SELECT delivery_date, gallons, address, delivery_date, price, total FROM fuel_quote_history WHERE username = ?");
+        $stmt->bind_param("s", $username);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                $fuelQuotes[] = $row;
+            }
+        }
+        $stmt->close();
+        return $fuelQuotes;
+    }
+}
+
 $fuelQuotes = [];
 if (isset($_SESSION['username'])) {
   if(!isset($conn)){
