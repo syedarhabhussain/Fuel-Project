52b8edc06cf16959c24a9c9b011146deb1f1566f
syedarhabhussain <syedarhabhussain>
2024-04-11 14:29:35 +0500

    Update database connection

--- tests/dbTest.php
+++ tests/dbTest.php
@@ -5,9 +5,18 @@ class DBTest extends TestCase
 {
     public function testDBInclude()
     {
-        ob_start();
-        include __DIR__ . '/../src/db.php';
-        $output = ob_get_clean();
-        $this->assertStringContainsString('<?php', $output);
+        $mysqli = $this->createMock(mysqli::class);
+        $mysqli->expects($this->once())
+               ->method('__construct')
+               ->with('localhost', 'root', '', 'fuel_quote')
+               ->willReturn(null);
+
+        $mysqli->connect_error = null;
+        $connector = new DatabaseConnector();
+        $reflection = new ReflectionClass(DatabaseConnector::class);
+        $method = $reflection->getMethod('connect');
+        $method->setAccessible(true);
+        $result = $method->invokeArgs($connector, []);
+        $this->assertInstanceOf(mysqli::class, $result);
     }
 }
