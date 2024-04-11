7b03d2338715ddb4f568ef540127110a1aa974cd
syedarhabhussain <syedarhabhussain>
2024-04-11 14:34:03 +0500

    Update tests

--- src/db.php
+++ src/db.php
@@ -1,14 +1,11 @@
 <?php
-class DatabaseConnector {
-    public function connect() {
-        $servername = "localhost";
-        $username = "root";
-        $password = "";
-        $dbname = "fuel_quote";
-        $conn = new mysqli($servername, $username, $password, $dbname);
-        if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
-        }
-        return $conn;
-    }
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "fuel_quote";
+
+$conn = new mysqli($servername, $username, $password, $dbname);    
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
+?>
