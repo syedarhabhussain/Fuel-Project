9f6133cca2f7a3ab84c29ef05136af4b5bbeba3f
syedarhabhussain <syedarhabhussain>
2024-04-11 16:19:22 +0500

    Update get history test

--- tests/getHistoryTest.php
+++ tests/getHistoryTest.php
@@ -1,5 +1,6 @@
 <?php
 use PHPUnit\Framework\TestCase;
+
 require_once __DIR__ . '/../src/db.php';
 
 class GetHistoryTest extends TestCase
@@ -8,14 +9,32 @@ class GetHistoryTest extends TestCase
     {
         global $conn;
         $_SESSION['username'] = 'testUser';
-        $conn = $this->createMock(mysqli::class);
+        
+        $result = $this->createMock(mysqli_result::class);
+        $result->method('fetch_assoc')->will($this->onConsecutiveCalls([
+            'delivery_date' => '2021-01-01', 
+            'gallons' => 100, 
+            'address' => '123 Main St', 
+            'price' => 2.50, 
+            'total' => 250
+        ], null));
+        $result->num_rows = 1;
+
         $stmt = $this->createMock(mysqli_stmt::class);
+        $stmt->method('bind_param')->willReturn(true);
+        $stmt->method('execute')->willReturn(true);
+        $stmt->method('get_result')->willReturn($result);
+        
+        $conn = $this->getMockBuilder(mysqli::class)
+                     ->disableOriginalConstructor()
+                     ->getMock();
         $conn->method('prepare')->willReturn($stmt);
-        $stmt->method('get_result')->willReturn(5);
+        
         ob_start();
         include __DIR__ . '/../src/get_history.php';
         $output = ob_get_clean();
-        $this->assertStringContainsString('session_start();', $output);
+        $this->assertNotEmpty($fuelQuotes);
+        // $this->assertStringContainsString('session_start();', $output);
         session_unset();
     }
 }
